3501f1f546d709a5d0f6763f9991afd00547b6ee
LibRusty <161581102+LibRusty@users.noreply.github.com>
2024-12-20 14:50:17 +0700

    Pushed

---
+++ tsu_module.c
@@ -0,0 +1,22 @@
+#include <linux/module.h> 
+#include <linux/kernel.h> 
+#include <linux/init.h>    
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("LibRust");
+MODULE_DESCRIPTION("A simple Linux kernel module for Tomsk State University");
+
+static int __init tsu_module_init(void)
+{
+    pr_info("Welcome to the Tomsk State University\n");
+    return 0;
+}
+
+
+static void __exit tsu_module_exit(void)
+{
+    pr_info("Tomsk State University forever!\n");
+}
+
+module_init(tsu_module_init);
+module_exit(tsu_module_exit);
